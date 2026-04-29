b155e480572b2b0daf504dc5b273f0c9d10c2561
tvcorporativafluidsystem <tvcorporativafluidsystem@gmail.com>
2026-04-29 01:02:17 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    // !! ATENÇÃO !!
+    // Isso permite que o build termine mesmo que haja erros de TypeScript.
+    ignoreBuildErrors: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
